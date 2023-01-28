d6a81a55082a2785260c903b0d3177c95ae00855
dendy_prtha <dendy.prtha@ti.ukdw.ac.id>
2023-01-28 07:28:59 +0700

    add test example

---
+++ src/main/java/com/ug9/eTransactionProject/ThreeRandoms.java
@@ -0,0 +1,15 @@
+package com.ug9.eTransactionProject;
+
+public class ThreeRandoms {
+    public static String helloYou(String blank) {
+        return "Blank";
+    }
+
+    public static int countDogs(String catCatCatcat) {
+        return 0;
+    }
+
+    public boolean flipFlop(int i, int i1) {
+        return false;
+    }
+}
